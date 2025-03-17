e437b3fde0c8deb9ad11cb0b10fd04683548695d
mill-del <malika.adeyeva@narxoz.kz>
2025-03-17 09:33:36 +0500

    Initial commit: Added Online Payment Gateway project

---
+++ src/CryptoPayment.java
@@ -0,0 +1,6 @@
+public class CryptoPayment implements PaymentMethod{
+    @Override
+    public void processPayment(double amount){
+        System.out.println("Processing cryptocurrency payment of $" + amount);
+    }
+}
---
+++ src/PaymentFactory.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PaymentFactory {
+    public static PaymentMethod getPaymentMethod(String type) {
+        switch (type.toLowerCase()) {
+            case "creditcard":
+                return new CreditCardPayment();
+            case "paypal":
+                System.out.println("Введите email для PayPal:");
+                Scanner scanner = new Scanner(System.in);
+                String email = scanner.nextLine();
+                return new PayPalAdapter(email);
+            case "crypto":
+                return new CryptoPayment();
+            default:
+                throw new IllegalArgumentException("Неверный метод оплаты!");
+        }
+    }
+}
